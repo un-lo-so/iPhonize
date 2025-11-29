37c868a55b570c0b608760a9c86424179c09cfab
un-lo-so <un-lo-so@localhost>
2025-11-30 00:49:56 +0100

    First commit

---
+++ iPhonize.py
@@ -0,0 +1,48 @@
+class contact:
+	def __init__(self):
+		name=""
+		displayname=""
+		nickname=""
+		uid=""
+		
+		# Handling emails
+		emailtypes=[]
+		emailaddr=[]
+		email_index=0
+		
+		# Handling URLs
+		urltype=[]
+		urladdr=[]
+		url_index=0
+		
+		# Handling addresses
+		addrtype=[]
+		address=[]
+		addr_index=0
+		
+		# Custom fields
+		custom1=""
+		custom2=""
+		custom3=""
+		custom4=""
+		
+		# Handling telephone
+		teltype=[]
+		tel=[]
+		
+		timezone=""
+		
+		birthday=""
+		anniversary=[]
+		
+		note=""
+		
+		# Organization property
+		title=""
+		role=""
+		org_name=""
+		department=""
+		
+		# Instant messaging
+		im=[]
+		
